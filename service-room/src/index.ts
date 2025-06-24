494361f32671ee49d7b8465dcd43bff4f1d5b170
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-06-24 10:14:31 +0200

    Add blank room and team services so docker can run

---
+++ service-room/src/index.ts
@@ -0,0 +1,28 @@
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+// Logging requests
+app.use(morgan('combined'));
+
+// CORS
+app.use(cors());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('/ of API of service-room');
+})
+
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Service ROOM is running at http://localhost:${port}`);
+});
