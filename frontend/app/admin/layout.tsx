5e35732617d0a32f620042422f3b94e2b0a92ae2
Yoan <mrts.yoan@gmail.com>
2025-06-25 12:04:15 +0200

    feat(front-navbar): add sidebar

---
+++ frontend/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ frontend/components/nav/sidebar-content.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Users, DoorOpen, UserRoundCog } from "lucide-react";
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import Link from "next/link";
+
+export function SidebarContentSections() {
+  return (
+    <>
+      <SidebarGroup>
+        <SidebarGroupLabel>Gestion des Utilisateurs</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/admin/students">
+                <Users className="h-5 w-5" />
+                <span>Étudiants</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+
+      <SidebarGroup>
+        <SidebarGroupLabel>Organisation de l'Établissement</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/admin/rooms">
+                <DoorOpen className="h-5 w-5" />
+                <span>Salles</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/admin/groups">
+                <UserRoundCog className="h-5 w-5" />
+                <span>Groupes</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+
+      <div className="mt-auto p-4 flex justify-center group-has-data-[collapsible=icon]/sidebar-wrapper:hidden">
+        <img src="/logo-room-managment.png" alt="App Logo" className="h-30 w-auto" />
+      </div>
+    </>
+  );
+}
