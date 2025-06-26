e4bcc7b123f5faae0d570306cf7a625e26c49450
Yoan <mrts.yoan@gmail.com>
2025-06-26 11:46:02 +0200

    Add swagger

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,9 +10,21 @@ const SERVICE_USER_URL = process.env.SERVICE_USER_URL || "http://localhost:7000"
 const SERVICE_ROOM_URL = process.env.SERVICE_ROOM_URL || "http://localhost:7001";
 const SERVICE_TEAM_URL = process.env.SERVICE_TEAM_URL || "http://localhost:7002";
 
+const swaggerUiOptions = {
+  customCss: '.swagger-ui .topbar { display: none }',
+};
+app.use(
+  '/documentation',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerUiOptions)
+);
+
 app.use(express.json());
 
 app.use((req: express.Request, _res: express.Response, next: express.NextFunction) => {
+  if (req.path.startsWith('/api-docs')) {
+    return next();
+  }
   console.log(`[API Gateway] ${req.method} ${req.path}`);
   next();
 });
